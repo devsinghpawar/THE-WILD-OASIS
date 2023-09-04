f8f88932f6a2b30e068000e265b52ad629b76c7e
Dev singh pawar <pawarsinghdev@gmail.com>
2023-09-04 22:02:50 +0530

    connecting Supabase and setting up React Query

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://qbubbtvmzxwxplwxtbtv.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFidWJidHZtenh3eHBsd3h0YnR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM4MDIyNDIsImV4cCI6MjAwOTM3ODI0Mn0.sqrmMLl5Er3aFGer4CRn2TdVto6tKV5kH_Yu2jto_YA";
+const supabase = createClient(supabaseUrl, supabaseKey);
+export default supabase;
