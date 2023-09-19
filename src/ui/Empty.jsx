92df864e9d34dd507179c23dee3ddfa07cb4ca03
Dev singh pawar <pawarsinghdev@gmail.com>
2023-09-19 15:01:46 +0530

    Update Empty file

--- src/ui/Empty.jsx
+++ src/ui/Empty.jsx
@@ -1,5 +1,13 @@
+import styled from "styled-components";
+
+const ParaGroup = styled.p`
+  display: flex;
+  padding: 5rem;
+  font-size: 25px;
+`;
+
 function Empty({ resourceName }) {
-  return <p>No {resourceName} could be found.</p>;
+  return <ParaGroup>No {resourceName} could be found.</ParaGroup>;
 }
 
 export default Empty;
